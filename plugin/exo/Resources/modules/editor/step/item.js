b6cac3bcbe7e2058abf4f9f29455eeb274a802e9
Stéphane Klein <stephaneklein221@gmail.com>
2016-07-14 11:36:00 +0200

    Refactor reducers with immutability helper

--- plugin/exo/Resources/modules/editor/step/item.js
+++ plugin/exo/Resources/modules/editor/step/item.js
@@ -1,4 +1,5 @@
 import {assert} from './../util'
+import update from 'immutability-helper'
 
 export const ITEM_CREATE = 'ITEM_CREATE'
 export const ITEM_DELETE = 'ITEM_DELETE'
@@ -28,29 +29,16 @@ export const itemActions = {
 
 export const itemReducers = {
   [ITEM_CREATE]: (state, action) => {
-    const step = state.find(step => step.id === action.stepId)
-    const index = state.indexOf(step)
-    const newStep = {
-      id: step.id,
-      items: [...step.items, {
-        id: `tmp-${Date.now()}`,
-        title: 'New item',
-        type: action.itemType
-      }]
-    }
-    return [...state.slice(0, index), newStep, ...state.slice(index + 1)]
+    const stepIndex = state.findIndex(step => step.id === action.stepId)
+    return update(state, {[stepIndex]: {items: {$push: [{
+      id: `tmp-${Date.now()}`,
+      title: 'New item',
+      type: action.itemType
+    }]}}})
   },
   [ITEM_DELETE]: (state, action) => {
-    const step = state.find(step => step.id === action.stepId)
-    const index = state.indexOf(step)
-    const itemIndex = step.items.findIndex(item => item.id === action.itemId)
-    const newStep = {
-      id: step.id,
-      items: [
-        ...step.items.slice(0, itemIndex),
-        ...step.items.slice(itemIndex + 1)
-      ]
-    }
-    return [...state.slice(0, index), newStep, ...state.slice(index + 1)]
+    const stepIndex = state.findIndex(step => step.id === action.stepId)
+    const itemIndex = state[stepIndex].items.findIndex(item => item.id === action.itemId)
+    return update(state, {[stepIndex]: {items: {$splice: [[itemIndex, itemIndex]]}}})
   }
 }
